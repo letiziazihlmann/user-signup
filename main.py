779696cbafcaf1e4b1b9268d80d61c730d94b133
Letizia Zihlmann <lzihlmann@msn.com>
2017-03-30 19:26:20 -0500

    a few more lines of code, issue with post in main handler

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 import webapp2
+import re
 
 page_header = """
 <!DOCTYPE html>
@@ -42,39 +43,42 @@ page_footer = """
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         body = '''
-        <form method='post'>
+        <form action='/welcome' method='post'>
             <table>
                 <tbody>
                     <tr>
                         <td>
-                            <label for='Username'>Username</label>
+                            <label for='username'>Username</label>
                         </td>
                         <td>
-                            <input name='Username' type='text' value required>
-                        </td>
-                    <tr>
-                    <tr>
-                        <td>
-                            <label for='Password'>Password</label>
-                        </td>
-                        <td>
-                            <input name='password' type='password' value required>
+                            <input name='username' type='text' value required>
+                            <span class='error'></span>
                         </td>
                     <tr>
                     <tr>
                         <td>
-                            <label for='Verify'>Verify Password</label>
+                            <label for='password'>Password</label>
                         </td>
                         <td>
-                            <input name='verify' type='password' value required>
+                            <input name='password' type='password' value="" value required>
                         </td>
                     <tr>
                     <tr>
                         <td>
-                            <label for='Email'>Email</label>
+                            <label for='verify'>Verify Password</label>
                         </td>
                         <td>
-                            <input name='email' type='email' value required>
+                            <input name='verify' type='password' value="" value required>
+                            <span class='error'></span>
+                        </td>
+                    <tr>
+                    <tr>
+                        <td>
+                            <label for='email'>Email</label>
+                        </td>
+                        <td>
+                            <input name='email' type='email'>
+                            <span class='error'></span>
                         </td>
                     </tr>
             </table>
@@ -82,9 +86,47 @@ class MainHandler(webapp2.RequestHandler):
         </form>
         '''
 
+        error = self.request.get("error")
+        if error:
+            error_esc = cgi.escape(error, quote=True)
+            error_element = '<p class="error">' + error_esc + '</p>'
+        else:
+            error_element = ''
+
         content = page_header + body + page_footer
         self.response.write(content)
 
+        def post(self):
+            #this will pull all of the values entered for the above inputs and assign them to a variable that i can use to test
+            # if the input is valid
+            entered_username = self.request.get('username')
+            entered_password = self.request.get('password')
+            entered_verify = self.request.get('verify')
+            entered_email = self.request.get('email')
+
+            #starting to validate the input, the re_username.match(entered_username) should be a boolean so true or false
+            re_username = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+            def valid_username(entered_username):
+                username_valid = re_username.match(entered_username)
+                return username_valid
+
+            #test to see if the username is valid if not, line 109 will redirect to line 89
+            if username_valid == False:
+                username_error = "Please enter a valid username"
+                self.redirect("/?error=" + username_error)
+
+
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+
+    def post(self):
+        username = self.request.get('username')
+        welcome_body = "<h1>Welcome " + username + "</h1>"
+        self.response.write(welcome_body)
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
